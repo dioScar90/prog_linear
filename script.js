d68c324bfea942c78e057bc814ae5b3b46340449
Diogo L Scarmagnani <diogols@live.com>
2023-06-02 12:22:01 -0300

    backward forward

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
-const button = document.querySelector(`[data-js="button-slide-in"]`);
+const ulBtnNav = document.querySelector(`[data-js="ul-footer"]`);
 
-const reloadSlide = e => {
-    button.classList.remove("slide-in");
-    setTimeout(() => button.classList.add("slide-out"), 50);
+const slide = (toLeft = false) => {
+  const back = toLeft === true ? "-back" : '';
+
+  const btnToShow = document.querySelector(`:is(.slide-out, .slide-back-out)`);
+  const btnToHide = document.querySelector(`:is(.slide-in, .slide-back-in)`);
+
+  btnToShow.classList.remove("slide-in", "slide-back-in", "slide-out", "slide-back-out");
+  btnToHide.classList.remove("slide-in", "slide-back-in", "slide-out", "slide-back-out");
+
+  btnToHide.classList.add(`slide${back}-out`);
+  btnToShow.classList.add(`slide${back}-in`);
 }
 
-button.addEventListener("click", reloadSlide);
+const slideToRight  = () => slide();
+const slideToLeft   = () => slide(true);
+
+const verifyDataset = dataset => ({
+  "btn-left"    : slideToRight,
+  "btn-right"   : slideToLeft,
+})[dataset?.js] || false
+
+const startAfterClick = e => {
+  const navButton = e.target;
+
+  const funcToCall = verifyDataset(navButton.dataset);
+
+  if (!funcToCall)
+    return;
+  
+  funcToCall();
+}
+
+const verifyKey = key => ({
+  "ArrowRight"  : slideToRight,
+  "ArrowLeft"   : slideToLeft,
+})[key] || false
+
+const startAfterKeyup = e => {
+  const key = e.key;
+
+  const funcToCall = verifyKey(key);
+
+  if (!funcToCall)
+    return;
+  
+  funcToCall();
+}
+
+ulBtnNav.addEventListener("click", startAfterClick);
+document.addEventListener("keyup", startAfterKeyup);
